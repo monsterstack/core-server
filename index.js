f8187723858066dfe120cc9b1449c526978feeb6
zacharyrote <zachary.rote@gmail.com>
2017-02-02 15:31:04 -0500

    add encapsulated cluster

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const appRoot = require('app-root-path');
 const bodyParser = require('body-parser');
 const bearerToken = require('express-bearer-token');
 
-const Cluster = require('./libs/cluster');
+const Cluster = require('./libs/cluster').Cluster;
 const AuthCheckMiddleware = require('security-middleware').AuthCheckMiddleware;
 const RealizationCheckMiddleware = require('discovery-middleware').RealizationCheckMiddleware;
 
