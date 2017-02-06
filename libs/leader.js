eaeb18f0f228bada6947c9d7ec7e29be84ce0d3e
zacharyrote <zachary.rote@gmail.com>
2017-02-06 09:51:59 -0500

    throw error if redis cli is not set

--- libs/leader.js
+++ libs/leader.js
@@ -11,7 +11,7 @@ class Leader {
     this.redisCli = redisCli;
     this.options = options;
     if(this.redisCli)
-      this.me = new Bronto({client: this.redisCli});
+      this.me = new Bronto({client: this.redisCli, subscriber: this.redisCli});
     else {
       throw new Error("Missing Redis Client");
     }
