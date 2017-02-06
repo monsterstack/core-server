345fade5a4c65b2f0129d93752235106b8920766
zacharyrote <zachary.rote@gmail.com>
2017-02-06 12:19:40 -0500

    look up public ip

--- libs/server.js
+++ libs/server.js
@@ -72,10 +72,11 @@ class Server {
     };
 
     let p = new Promise((resolve, reject) => {
-      let ip = require('ip');
-      console.log(ip.address());
-      descriptor.endpoint = "http://"+ip.address()+":"+config.port
-      resolve(descriptor);
+      let ip = require('public-ip');
+      ip.v4().then((ip) => {
+        descriptor.endpoint = "http://"+ip.address()+":"+config.port
+        resolve(descriptor);
+      })
     });
     return p;
 
