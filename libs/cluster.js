9cafcf98a2eb74b451b8e914eee0f918cfdc47b6
zacharyrote <zachary.rote@gmail.com>
2017-02-15 16:26:58 -0500

    fix

--- libs/cluster.js
+++ libs/cluster.js
@@ -92,7 +92,7 @@ class Cluster {
 
     let p = new Promise((resolve, reject) => {
       let ip = require('ip').address();
-      console.log(`HOST IP FROM env is ${process.env.HOST_IP}`)
+      // console.log(`HOST IP FROM env is ${process.env.HOST_IP}`)
       // if(process.env.HOST_IP)
       //   ip = process.env.HOST_IP;
       descriptor.endpoint = "http://"+ip+":"+myPort;
