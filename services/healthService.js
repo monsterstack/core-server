8a6b5d2c696d6621b134bf5b9be48c47c478648e
zacharyrote <zachary.rote@gmail.com>
2017-02-21 14:47:13 -0500

    core-server

--- services/healthService.js
+++ services/healthService.js
@@ -6,20 +6,51 @@ class HealthService {
     this.os = require('os');
   }
 
+  
+  _cpuAverage() {
+    let self = this;
+
+    let cpus = self.os.cpus();
+    let totalTick = 0;
+    let totalIdle = 0;
+
+    cpus.forEach((cpu) => {
+      for(let type in cpu.times) {
+        totalTick += cpu.times[type];
+      }
+
+      totalIdle += cpu.times.idle;
+    });
+
+    return {idle: totalIdle/cpus.length, total: totalTick/cpus.length};
+  }
+
   getHealth() {
     let self = this;
     let p = new Promise((resolve, reject) => {
       let loadAvg = self.os.loadavg();
-      let cpus = self.os.cpus();
-      let cpuMeasures = [];
-      cpus.forEach((cpu) => {
-        cpuMeasures.push(cpu.times);
+      let totalLoad = 0;
+      loadAvg.forEach((loadAvg) => {
+        totalLoad = totalLoad + loadAvg;
       });
 
-      resolve({
-        loadAvg: loadAvg,
-        cpus: cpus
-      })
+      let finalLoadAvg = totalLoad/(loadAvg.length);
+
+      let cpuFirstMeasure = this._cpuAverage();
+
+      setTimeout(() => {
+        let cpuSecondMeasure = this._cpuAverage();
+
+        let idleDiff = cpuSecondMeasure.idle - cpuFirstMeasure.idle;
+        let totalDiff = cpuSecondMeasure.total - cpuFirstMeasure.total;
+
+        let percentageCPU = 100 - ~~(100 * (idleDiff/totalDiff));
+
+        resolve({
+          loadAvg: finalLoadAvg,
+          cpuPercentAvg: percentageCPU
+        });
+      }, 100);
     });
 
     return p;
---
+++ tests/health-service-test.js
@@ -0,0 +1,49 @@
+'use strict';
+const HealthService = require('../services/healthService.js');
+
+/**
+ * Health Service Test
+ */
+describe('health-service', (done) => {
+    let healthService = null;
+    before((done) => {
+        healthService = new HealthService();
+        done();
+    });
+
+    it('Health calculation has loadAvg', (done) => {
+        healthService.getHealth().then((health) => {
+            console.log(health);
+            if(health.loadAvg) {
+                if(typeof health.loadAvg === 'number')
+                    done();
+                else
+                    done(new Error('Invalid loadAvg type, expecting `number`'));
+            } else {
+                done(new Error('Missing loadAvg'));
+            }
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    it('Health calculation has cpuAvg', (done) => {
+        healthService.getHealth().then((health) => {
+            console.log(health);
+            if(health.cpuPercentAvg) {
+                if(typeof health.cpuPercentAvg === 'number')
+                    done();
+                else
+                    done(new Error('Invalid cpuAvg type, expecting `number`'));
+            } else {
+                done(new Error('Missing cpuAvg'));
+            }
+        }).catch((err) => {
+            done(err);
+        });
+    });
+
+    after((done) => {
+        done();
+    });
+});
