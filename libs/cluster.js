03f243f18ba156f8df20a984b582a6a561aabf2c
zacharyrote <zachary.rote@gmail.com>
2017-02-21 13:28:09 -0500

    core-server

--- libs/cluster.js
+++ libs/cluster.js
@@ -270,21 +270,6 @@ class Cluster extends Node {
       
     }
   }
-
-  // _emitProxyReady(proxy) {
-  //   this.emit('proxy.ready', proxy);
-  // }
-
-  // /**
-  //  * Redis Retry strategy
-  //  */
-  // _redisRetryStrategy() {
-  //   return (options) => {
-  //     console.log(options);
-  //     // reconnect after
-  //     return Math.min(options.attempt * 100, 3000);
-  //   }
-  // }
 }
 
 // Public
