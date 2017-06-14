a9a5ee261829c12acff2085e776104b6f756fc93
Zachary Rote <zacharyrote@Zacharys-MacBook-Pro.local>
2017-06-14 12:44:03 -0400

    api secret factory

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Validator = require('./libs/validator');
 const Server = require('./libs/server').Server;
 const Cluster = require('./libs/cluster').Cluster;
+const ApiSecretFactory = require('./libs/apiSecretFactory').ApiSecretFactory;
 
 const AuthCheckMiddleware = require('security-middleware').AuthCheckMiddleware;
 const RealizationCheckMiddleware = require('discovery-middleware').RealizationCheckMiddleware;
@@ -14,6 +15,7 @@ module.exports.HealthService = require('./libs/services/healthService');
 module.exports.SwaggerService = require('./libs/services/swaggerService');
 module.exports.ProxyCacheService = require('./libs/services/proxyCacheService');
 module.exports.Cluster = Cluster;
+module.exports.ApiSecretFactory = ApiSecretFactory;
 module.exports.Server = Server;
 
 module.exports.Validator = Validator;
